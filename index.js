e51fb3c424aec16dc2affd45423fbc87841e0182
daniel rangel <dr@daniels-MacBook-Pro.local>
2021-05-09 23:25:02 -0400

    debugging deployment failed

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app           = express();
 app.set             ('view engine', 'ejs')
 require             ('./database/connection')
 app.use             ('', require('./controllers/routes'))
-app.use          (express.static(__dirname + '/public'));
+app.use             (express.static(__dirname + '/public'));
 
 let port = 5000;
 
